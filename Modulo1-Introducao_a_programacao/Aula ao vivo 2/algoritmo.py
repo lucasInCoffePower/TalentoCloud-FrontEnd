f793f9d16a7d3f95c0e07411ed25f65ae427043a
YoursTrully (YT) <52331590+lucasInCoffePower@users.noreply.github.com>
2023-07-28 18:58:57 -0300

    Create algoritmo.py

---
+++ Modulo1-Introducao_a_programacao/Aula ao vivo 2/algoritmo.py
@@ -0,0 +1,31 @@
+def main():
+  numero = 5
+  print(numero)
+  print(type(numero))
+
+  
+  bebidaTipo, bebidaValor, almocoTipo, almocoValor, sobremesaTipo, sobremesaValor ='vinho', 10, 'filé de peito', 20.00, 'creme de amendoin', 10.00
+  orcamento = 200
+  qtdConvidados = 4
+  
+  
+  conta=(bebidaValor*3)+(almocoValor*4)+(sobremesaValor*2)
+  print('O valor total é:',total)
+  
+  rachaPessoa=conta/qtdConvidados
+  print('Valor por pessoa:', rachaPessoas)
+  
+  bomAmigo = True
+  
+  if orcamento >= rachaPessoa):
+   print("Vamos dividir")
+  elif(bomAmigo):
+    print("Top")
+  else:
+   print("Não")
+
+  
+
+
+if __name__ == '__main__':
+  main()
