bd185c8efb1de447824e5a2b86cb0f761b4035be
YoursTrully (YT) <52331590+lucasInCoffePower@users.noreply.github.com>
2023-07-31 03:28:04 -0300

    Update calculadora.py

--- Modulo1-Introducao_a_programacao/Desafio3/Aula2/CodePark/calculadora.py
+++ Modulo1-Introducao_a_programacao/Desafio3/Aula2/CodePark/calculadora.py
@@ -17,22 +17,22 @@ def menuOperacoes():
 def calculadora():
     '''Recebe operação e operandos, e calcula resultados'''
     menuOperacoes()
-    operacao = int(input())
+    operacao = input()
     
-    if operacao == 5:
+    if operacao == '5':
         return False
-    elif operacao != 1 and operacao != 2 and operacao != 3 and operacao != 4:
+    elif operacao != '1' and operacao != '2' and operacao != '3' and operacao != '4':
         print('Operação inválida')
     else:
         numero1 = float(input('Digite o primeiro número: '))
         numero2 = float(input('Digite o segundo número: '))
-        if operacao == 1:
+        if operacao == '1':
             soma = numero1+numero2
             print('{}+{}={}'.format(numero1, numero2,soma))
-        elif operacao == 2:
+        elif operacao == '2':
             diferenca = numero1-numero2
             print('{}-{}={}'.format(numero1, numero2, diferenca))
-        elif operacao == 3:
+        elif operacao == '3':
             produto = numero1*numero2
             print('{}x{}={}'.format(numero1, numero2, produto))
         else:
