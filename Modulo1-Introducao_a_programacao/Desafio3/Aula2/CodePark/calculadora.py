c75b38f6aab975168f31b4b1ac21433d404b7bd8
YoursTrully (YT) <52331590+lucasInCoffePower@users.noreply.github.com>
2023-07-31 04:13:40 -0300

    Update calculadora.py

--- Modulo1-Introducao_a_programacao/Desafio3/Aula2/CodePark/calculadora.py
+++ Modulo1-Introducao_a_programacao/Desafio3/Aula2/CodePark/calculadora.py
@@ -1,10 +1,4 @@
 # Calculadora
-operacoes = {
-    '1':'Soma'
-    '2':'Subtração'
-    '3':'Multiplicação'
-    '4':'Divisão'
-}
 
 def adicao(n1:float, n2:float):
     '''resultado da soma'''
@@ -16,7 +10,7 @@ def subtracao(n1:float, n2:float):
     return n1-n2
 
 
-def multiplicação(n1:float, n2:float):
+def multiplicacao(n1:float, n2:float):
     '''multiplicação'''
     return n1*n2
 
@@ -26,12 +20,20 @@ def divisao(n1:float, n2:float):
     return 'indeterminação' if n2 == 0 else n1/n2 
 
 
+operacoes = {
+    '1': (adicao, '+'),
+    '2': (subtracao, '-'),
+    '3': (multiplicacao, 'x'),
+    '4': (divisao, '÷')
+}
+
+
 def calculadora():
     '''Apresenta e recebe operação, recebe operandos, e calcula resultados'''
     
     print('''
         \nEscolha a operação prosseguir
-        1- Soma
+        1- Adição
         2- Subtração
         3- Multiplicação
         4- Divisão
@@ -42,20 +44,11 @@ def calculadora():
     
     if operacao not in operacoes:
         print('Essa operação não existe !')
-    elif operacao == '5':
-        return False
-    else:
-        numero1 = float(input('Digite o primeiro número: '))
-        numero2 = float(input('Digite o segundo número: '))
-        if operacao == '1':
-            print('{}+{}={}'.format(numero1, numero2, adicao(numero1, numero2)))
-        elif operacao == '2':
-            print('{}-{}={}'.format(numero1, numero2, subtracao(numero1, numero2)))
-        elif operacao == '3':
-            print('{}x{}={}'.format(numero1, numero2, multiplicacao(numero1, numero2)))
-        else:
-            print('{}/{}={}'.format(numero1, numero2, divisao(numero1, numero2)))
-    return True
+    elif operacao != '5':
+        num1 = float(input('Digite o primeiro número'))
+        num2 = float(input('Digite o segundo número'))
+        resultado = operacoes[operacao]
+        print('{}{}{}={}'.format(num1, resultado[1], num2, resultado[0](num1, num2)))
         
         
 def main():
