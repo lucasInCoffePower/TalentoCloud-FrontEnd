9d542a8ec266043a8db2997d140ee0d83a3ea0e8
YoursTrully (YT) <52331590+lucasInCoffePower@users.noreply.github.com>
2023-07-31 03:53:43 -0300

    Update calculadora.py

--- Modulo1-Introducao_a_programacao/Desafio3/Aula2/CodePark/calculadora.py
+++ Modulo1-Introducao_a_programacao/Desafio3/Aula2/CodePark/calculadora.py
@@ -1,4 +1,30 @@
 # Calculadora
+operacoes = {
+    '1':'Soma'
+    '2':'Subtração'
+    '3':'Multiplicação'
+    '4':'Divisão'
+}
+
+def adicao(n1:float, n2:float):
+    '''resultado da soma'''
+    return n1+n2
+
+
+def subtracao(n1:float, n2:float):
+    '''operação de subtração'''
+    return n1-n2
+
+
+def multiplicação(n1:float, n2:float):
+    '''multiplicação'''
+    return n1*n2
+
+
+def divisao(n1:float, n2:float):
+    '''divisao'''
+    return 'indeterminação' if n2 == 0 else n1/n2 
+
 
 def calculadora():
     '''Apresenta e recebe operação, recebe operandos, e calcula resultados'''
@@ -14,25 +40,21 @@ def calculadora():
     
     operacao = input()
     
-    if operacao == '5':
-        return False
-    elif operacao != '1' and operacao != '2' and operacao != '3' and operacao != '4':
+    if operacao not in operacoes:
         print('Essa operação não existe !')
+    elif operacao == '5':
+        return False
     else:
         numero1 = float(input('Digite o primeiro número: '))
         numero2 = float(input('Digite o segundo número: '))
         if operacao == '1':
-            soma = numero1+numero2
-            print('{}+{}={}'.format(numero1, numero2,soma))
+            print('{}+{}={}'.format(numero1, numero2, adicao(numero1, numero2)))
         elif operacao == '2':
-            diferenca = numero1-numero2
-            print('{}-{}={}'.format(numero1, numero2, diferenca))
+            print('{}-{}={}'.format(numero1, numero2, subtracao(numero1, numero2)))
         elif operacao == '3':
-            produto = numero1*numero2
-            print('{}x{}={}'.format(numero1, numero2, produto))
+            print('{}x{}={}'.format(numero1, numero2, multiplicacao(numero1, numero2)))
         else:
-            razao = 'Indeterminação' if numero2 == 0 else numero1/numero2
-            print('{}/{}={}'.format(numero1, numero2, razao))
+            print('{}/{}={}'.format(numero1, numero2, divisao(numero1, numero2)))
 
     return True
         
