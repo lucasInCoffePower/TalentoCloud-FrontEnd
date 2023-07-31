cafcd96cf19a8a474c5cc8cc690eb9e3a20062a6
YoursTrully (YT) <52331590+lucasInCoffePower@users.noreply.github.com>
2023-07-31 13:52:16 -0300

    upload idade.py

---
+++ Modulo1-Introducao_a_programacao/Desafio3/Aula3/CodePark/novo_teste.py
@@ -0,0 +1,59 @@
+
+
+from datetime import datetime  # biblioteca para manipular datas 
+import os
+
+
+def limparTela():
+    if os.name == 'nt':
+        # limpar cli Windows
+        os.system('cls')
+    else:
+        # limpar cli MacOs e Linux
+        os.system('clear')
+
+
+def receberAnoNascimento():
+    try:
+        ano_nascimento = int(input('Digite o ano em que você nasceu: '))
+    except ValueError as ve:
+        raise ValueError('Digitou texto ao invés de número !')
+    else:
+        return ano_nascimento
+
+
+def receberNome():
+    nome = input('Digite o seu nome: ')
+    return nome
+
+
+def calcularIdade(ano_nascimento:int):
+    '''Calcula a idade no ano atual'''
+    ano_atual = datetime.today().year
+    idade = ano_atual-ano_nascimento
+    return idade, ano_atual
+
+
+def main():
+    
+    nome = receberNome()
+    while True:
+        try:
+            ano_nascimento = receberAnoNascimento()
+            idade, ano_atual = calcularIdade(ano_nascimento)
+        except ValueError as ve:
+            print('Um erro ocorreu!')
+            print('Erro: {}'.format(ve))
+            input('Digite a data de nascimento novamente! Pressione enter para continuar!')
+        except Exception as e:
+            print('Um erro ocorreu')
+            print('Erro: {}'.format(e))
+        else:
+            print(f'Olá, {nome} ! Em {ano_atual} você completa {idade} anos de idade')
+            break
+        limparTela()
+        
+
+
+if __name__ == '__main__':
+    main()
